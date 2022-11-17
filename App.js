dac2b46973a819e2da1f67c0a4121e5e168eade9
Maurício de Carvalho <mauriciocr223@gmail.com>
2022-11-16 21:00:36 -0300

    feat: using FlatList do display goals list

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -11,7 +19,7 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      { text: enteredGoalText, key: Math.random().toString() },
     ]);
   }
 
@@ -19,6 +27,7 @@ export default function App() {
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
+          placeholderTextColor={"#7c7c8a"}
           style={styles.textInput}
           placeholder="Your course goal"
           onChangeText={goalInputHandler}
@@ -26,9 +35,16 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <Text key={goal}>{goal}</Text>
-        ))}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -39,6 +55,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1,
+    backgroundColor: "#121214",
   },
   inputContainer: {
     flex: 1,
@@ -47,16 +64,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    borderBottomColor: "#202024",
   },
   textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
+    // borderWidth: 1,
+    // borderColor: "#cccccc",
+    backgroundColor: "#202024",
     width: "70%",
     marginRight: 8,
     padding: 8,
+    color: "white",
+    borderRadius: 2,
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 2,
+    backgroundColor: "gray",
   },
 });
